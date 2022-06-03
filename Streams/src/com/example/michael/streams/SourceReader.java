1dc27925cbc087fbb8763cae066e5c1a415036c0
Michael <michael.patel94@gmail.com>
2022-06-03 12:57:25 -0400

    Using Path to create a file object

--- Streams/src/com/example/michael/streams/SourceReader.java
+++ Streams/src/com/example/michael/streams/SourceReader.java
@@ -3,14 +3,28 @@ package com.example.michael.streams;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SourceReader {
 
     // main
     public static void main(String[] args) {
+        // Create file objects
+        String filename = "C:\\Users\\micha\\Desktop\\JavaProjects\\Java-Refresh\\Streams\\src\\com\\example\\michael\\streams\\data.txt";
+
+        // Create a file object
+        Path source = Paths.get(filename);
+
         try (
-                FileReader file = new FileReader("C:\\Users\\micha\\Desktop\\JavaProjects\\Java-Refresh\\Streams\\src\\com\\example\\michael\\streams\\data.txt");
+                // Create an input stream
+                FileReader file = new FileReader(source.toFile());
+
+                // Associate a buffer filter with input stream
                 BufferedReader buffer = new BufferedReader(file);
+                
                 )  // declare resources inside parentheses
         {
             boolean eof = false;
